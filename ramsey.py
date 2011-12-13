bcd1b696d212d96ccd9ca5277b2f1acbaaf8cbe8
Matt Wittmann <mcwitt@gmail.com>
2011-12-12 21:40:43 -0800

    Added more efficient sweep method.

--- ramsey.py
+++ ramsey.py
@@ -38,14 +38,32 @@ def clique_count(a, m):
 
     return count
 
-def indset_count(a, n):
-    '''Determines the number of n-independent sets in the graph represented by
-    the adjacency matrix a'''
+def restricted_clique_count(a, m, j, k):
+    N = len(a)
+    rverts = range(N)
+    rverts.remove(j)
+    rverts.remove(k)
+    count = 0
 
-    return clique_count(a==False, n)
+    for alpha in combinations(rverts, m-2):
+        alpha = list(alpha) + [j, k]
+        alpha.sort()
+        isclique = True
+        for u, v in combinations(alpha, 2):
+            if not a[u,v]:
+                isclique = False
+                break
+        if isclique:
+            count += 1
+
+    return count
 
 def energy(a, n, m):
-    return clique_count(a, m) + indset_count(a, n)
+    return clique_count(a, m) + clique_count(a==False, n)
+
+def restricted_energy(a, n, m, j, k):
+    return restricted_clique_count(a, m, j, k)\
+            + restricted_clique_count(a==False, n, j, k)
 
 def sweep(a, n, m, beta):
     'Sweep with brute-force energy delta calculation'
@@ -60,21 +78,31 @@ def sweep(a, n, m, beta):
         else:
             a[j,k] = not a[j,k]
 
+def sweep2(a, n, m, beta):
+    'Sweep with more efficient energy delta calculation'
+    verts = range(len(a))
+    for j, k in combinations(verts, 2):
+        e0 = restricted_energy(a, n, m, j, k)
+        a[j,k] = not a[j,k]
+        e1 = restricted_energy(a, n, m, j, k)
+        de = e1 - e0
+        if de > 0 and random.random() > exp(-beta*de):
+            a[j,k] = not a[j,k]
+
 def draw(a, out='graph'):
     'Make an EPS drawing of the graph'
     import pyx
     c = pyx.canvas.canvas()
     N = len(a)
-    for k in xrange(N):
-        for j in xrange(k):
-            if a[j,k]: color=pyx.color.rgb.red
-            else: color=pyx.color.rgb.blue
-            coords = lambda j: (5*sin(2*pi*j/N), 5*cos(2*pi*j/N))
-            xj, yj = coords(j)
-            xk, yk = coords(k)
-            c.stroke(pyx.path.line(xj, yj, xk, yk), [color])
-    for k in xrange(N):
-        x, y = coords(k)
+    verts = range(N)
+    xy = lambda j: (5*sin(2*pi*j/N), 5*cos(2*pi*j/N))
+    for j, k in combinations(verts, 2):
+        color = pyx.color.rgb.red if a[j,k] else pyx.color.rgb.blue
+        xj, yj = xy(j)
+        xk, yk = xy(k)
+        c.stroke(pyx.path.line(xj, yj, xk, yk), [color])
+    for k in verts:
+        x, y = xy(k)
         c.fill(pyx.path.circle(x, y, 0.2), [pyx.color.rgb.black])
 
     c.writeEPSfile(out)
