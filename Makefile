ae111884448dcc43be4a2161bba7bb87863774e0
Matt Wittmann <mcwitt@gmail.com>
2011-12-23 11:40:58 -0700

    Wrote function to compute binomial coefficients and started work on initialization code.

---
+++ Makefile
@@ -0,0 +1,12 @@
+dsfmt_dir = $(HOME)/local/src/dSFMT-src-2.1
+dsfmt_flags = -I$(dsfmt_dir) -DDSFMT_MEXP=2203 -DHAVE_SSE2
+
+VPATH = $(dsfmt_dir)
+#CFLAGS = -Wall -std=gnu99 -lm -O3 $(dsfmt_flags) -pg
+CFLAGS = -Wall -std=gnu99 -lm -O3 $(dsfmt_flags)
+OBJECTS = dSFMT.o
+
+.PHONY: ramsey
+
+ramsey: ramsey.c dSFMT.o
+	gcc -DDEBUG -DNV=$(NV) -DR=$R -DS=$S -o $@$(NV)-$R-$S.out $^ $(CFLAGS)
--- ramsey.c
+++ ramsey.c
@@ -1,21 +1,26 @@
 /*
  * Undefined constants
- *      NV: number of vertices
- *      NT_MAX: maximum number of temperatures
+ *      NV  : number of vertices
+ *      R   : blue clique size
+ *      S   : red clique size
  */
+
+#define NT_MAX 16
 
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <limits.h>
 #include "dSFMT.h"
 
+typedef unsigned long ULONG;
+
 /* Replica-specific variables ************************************************/
-rep_t[NT_MAX] reps; /* storage for parallel tempering (PT) replicas */
 typedef struct
 {
-    int am[N][N];   /* adjacency matrix */
+    int am[NV][NV];   /* adjacency matrix */
     int energy;
-} rep;
+} rep_t;
 
 /* Global variables **********************************************************/
 rep_t reps[NT_MAX]; /* storage for parallel tempering (PT) replicas */
@@ -30,6 +35,51 @@ int nswaps[NT_MAX]; /* number of swaps between each adjacent pair */
 dsfmt_t rstate;     /* state of random number generator */
 uint32_t rseed;
 
+/* binomial coefficient */
+ULONG binomial(ULONG n, ULONG k)
+{
+    ULONG r = 1, d = n - k; 
+
+    /* choose the smaller of k and n-k */
+    if (d > k) { k = d; d = n - k; }
+
+    while (n > k)
+    {
+        if (r >= ULONG_MAX / n) return 0;    /* overflown */
+        r *= n--;
+
+        /* divide as soon as possible to avoid overflow */
+        while (d > 1 && !(r % d)) r /= d--;
+    }
+
+    return r;
+}
+
+void init_reps(rep_t *reps, rep_t **preps)
+{
+    rep_t *p;
+    int iT, j, k, e0;
+
+    /* energy with all edges blue */
+    e0 = binomial(NV, R);
+
+    for (iT = 0; iT < nT; iT++)
+    {
+        p = &reps[iT];
+        preps[iT] = p;
+        p->energy = e0;
+
+        /* start with completely blue graph */
+        for (j = 0; j < NV; j++) {
+            for (k = 0; k < j; k++) {
+                p->am[j][k] = 1;
+                /* INITIALIZE NUMBER OF CLIQUES INVOLVING THIS EDGE */
+            }
+        }
+
+    }   /* end of loop over temperatures */
+}
+
 void sweep(rep_t **preps)
 {
     rep_t *p;
@@ -42,7 +92,7 @@ void sweep(rep_t **preps)
     }
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     FILE *infile;
     double t;
