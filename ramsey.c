6b8338afb3c259b0431decfe1a405eb523c38f8c
Matt Wittmann <mcwitt@gmail.com>
2012-01-31 15:37:13 -0800

    Added files.

---
+++ ramsey.c
@@ -0,0 +1,295 @@
+#include <stdio.h>
+#include "ramsey.h"
+
+dsfmt_t rng_state;
+
+/* subs[ei] (subr[ei]) lists the NSGFES (NSGFER) complete S-subgraphs
+ * (R-subgraphs) that include edge ei */
+int *subr[NED];
+int *subs[NED];
+ 
+/* edgs[si] (edgr[si]) lists the edges of S-subgraph (R-subgraph) si */
+int *edgr[NSGR];
+int *edgs[NSGS];
+
+int nedr = R*(R-1)/2;       /* number of edges in an R-subgraph */
+int neds = S*(S-1)/2;       /* number of edges in an S-subgraph */
+int nedsm1 = S*(S-1)/2 - 1; /* neds minus one */
+int nedsm2 = S*(S-1)/2 - 2;
+
+void init_tabs(int *sub[], int *edg[], int t, int nsgfe, int nedrs);
+void free_tabs(int *sub[], int *edg[], int nsg);
+
+void R_init(uint32_t seed)
+{
+    /* init random number generator */
+    dsfmt_init_gen_rand(&rng_state, seed);
+
+    init_tabs(subr, edgr, R, NSGFER, nedr);
+    init_tabs(subs, edgs, S, NSGFES, neds);
+}
+
+void R_finalize()
+{
+    free_tabs(subr, edgr, NSGR);
+    free_tabs(subs, edgs, NSGS);
+}
+
+void R_init_replica(rep_t *p)
+{
+    int j;
+
+    p->en = NSGS;
+    for (j = 0; j < NSGR; j++) p->nbr[j] = nedr;
+    for (j = 0; j < NSGS; j++) p->nbs[j] = neds;
+
+    for (j = 0; j < NED; j++)
+    {
+        p->sp[j] = 1;
+        p->h2[j] = -NSGFES;
+    }
+}
+
+void R_init_replica_random(rep_t *p)
+{
+    int j;
+
+    R_init_replica(p);
+
+    /* randomize spins, updating fields with each flip */
+    for (j = 0; j < NED; j++)
+    {
+        if (R_RAND() < 0.5)
+        {
+            p->sp[j] = -1;
+            p->en += p->h2[j];
+            R_update_fields(j, p->sp, p->nbr, p->nbs, p->h2);
+        }
+    }
+}
+
+void R_init_replica_from_file(rep_t *p, char filename[])
+{
+    FILE *fp;
+    int ned, sp, j;
+
+    R_init_replica(p);
+    fp = fopen(filename, "r");
+
+    if (! fscanf(fp, "%d", &ned)) 
+    {
+        fprintf(stderr, "error while reading %s\n", filename);
+        exit(1);
+    }
+
+    ned = ned*(ned-1)/2;
+    assert(ned < NED);
+
+    for (j = 0; j < NED - ned; j++)
+    {
+        if (R_RAND() < 0.5)
+        {
+            p->sp[j] = -1;
+            p->en += p->h2[j];
+            R_update_fields(j, p->sp, p->nbr, p->nbs, p->h2);
+        }
+    }
+
+    while (fscanf(fp, "%d", &sp) != EOF && j < NED)
+    {
+        if (sp == 0)
+        {
+            p->sp[j] = -1;
+            p->en += p->h2[j];
+            R_update_fields(j, p->sp, p->nbr, p->nbs, p->h2);
+        }
+
+        j++;
+    }
+
+    fclose(fp);
+}
+
+void R_update_fields(int ei, int sp[], int nbr[], int nbs[], int h2[])
+{
+    int si, j, ej, nbf;
+
+    if (sp[ei] == 1)
+    {
+        /* iterate over S-subgraphs including edge ei */
+        for (si = 0; si < NSGFES; si++)
+        {
+            nbf = nbs[subs[ei][si]] += 1;
+
+            if (nbf == neds)        /* flip created a blue clique */
+            {
+                h2[ei] += 1;
+                for (j = 0; j < neds; j++) h2[edgs[subs[ei][si]][j]] -= 1;
+            }
+            else if (nbf == nedsm1)
+            {
+                /* flip created an incomplete blue clique (one edge short) */
+                for (j = 0; j < neds; j++)
+                {
+                    ej = edgs[subs[ei][si]][j];
+                    if (sp[ej] == -1) { h2[ej] -= 1; break; }
+                }
+            }
+        }
+
+        /* iterate over R-subgraphs including edge ei */
+        for (si = 0; si < NSGFER; si++)
+        {
+            nbf = nbr[subr[ei][si]] += 1;
+
+            if (nbf == 1)       /* flip destroyed a red clique */
+            {
+                h2[ei] += 1;
+                for (j = 0; j < nedr; j++) h2[edgr[subr[ei][si]][j]] -= 1;
+            }
+            else if (nbf == 2)   /* flip destroyed an incomplete red clique */
+            {
+                for (j = 0; j < nedr; j++)
+                {
+                    ej = edgr[subr[ei][si]][j];
+                    if (sp[ej] == 1 && ej != ei) { h2[ej] -= 1; break; }
+                }
+            }
+        }
+    }
+    else
+    {
+        for (si = 0; si < NSGFER; si++)
+        {
+            nbf = nbr[subr[ei][si]] -= 1;
+
+            if (nbf == 0)       /* created a red clique */
+            {
+                h2[ei] -= 1;
+                for (j = 0; j < nedr; j++) h2[edgr[subr[ei][si]][j]] += 1;
+            }
+            else if (nbf == 1)  /* created an incomplete red clique */
+            {
+                for (j = 0; j < nedr; j++)
+                {
+                    ej = edgr[subr[ei][si]][j];
+                    if (sp[ej] == 1) { h2[ej] += 1; break; }
+                }
+            }
+        }
+
+        for (si = 0; si < NSGFES; si++)
+        {
+            nbf = nbs[subs[ei][si]] -= 1;
+
+            if (nbf == nedsm1)      /* destroyed a blue clique */
+            {
+                h2[ei] -= 1;
+                for (j = 0; j < neds; j++) h2[edgs[subs[ei][si]][j]] += 1;
+            }
+            else if (nbf == nedsm2) /* destroyed an incomplete blue clique */
+            {
+                for (j = 0; j < neds; j++)
+                {
+                    ej = edgs[subs[ei][si]][j];
+                    if (sp[ej] == -1 && ej != ei) { h2[ej] += 1; break; }
+                }
+            }
+        }
+    }
+}
+
+void R_save_graph(int sp[NED], char filename[])
+{
+    FILE *fp;
+    int i;
+
+    fp = fopen(filename, "w");
+    fprintf(fp, "%d\n", NV);
+    
+    for (i = 0; i < NED; i++)
+        fprintf(fp, "%d\n", (sp[i] == 1) ? 1 : 0);
+
+    fclose(fp);
+}
+
+void init_tabs(int *sub[], int *edg[], int t, int nsgfe, int nedrs)
+{
+    int ps[NED];    /* current positions in subgraph arrays */
+    int pe;         /* current position in edge array */
+    int c[t+2];     /* array of vertices of the current subgraph */
+    int ei, si;     /* edge index, subgraph index */
+    int j, k;
+
+    for (j = 0; j < NED; j++)
+    {
+        sub[j] = (int*) malloc(nsgfe * sizeof(int));
+        ps[j] = 0;
+    }
+
+    /* 
+     * iterate over all subgraphs with t vertices
+     */
+
+    /*
+     * algorithm to generate combinations adapted from Algorithm L in Knuth's
+     * Art of Computer Programming Vol. 4, Fasc. 3 (all-caps labels
+     * correspond to labels in the book)
+     */
+
+    /* INITIALIZE */
+    si = 0;
+    c[t] = NV;
+    c[t+1] = 0;
+    for (j = 0; j < t; j++) c[j] = j;
+
+    while (1)
+    {
+        /*
+         * VISIT combination c_1 c_2 ... c_t
+         * (algorithm guarantees that c_1 < c_2 < ... < c_t)
+         */
+
+        edg[si] = (int*) malloc(nedrs * sizeof(int));
+        pe = 0;
+
+        /* iterate over edges in this subgraph */
+        for (k = 0; k < t; k++)
+        {
+            for (j = 0; j < k; j++)
+            {
+                ei = c[k]*(c[k]-1)/2 + c[j];
+
+                /*
+                 * add subgraph si to list for edge ei
+                 * add edge ei to list for subgraph si
+                 */
+
+                sub[ei][ps[ei]++] = si;
+                edg[si][pe++] = ei;
+            }
+        }
+
+        si++;   /* finished with this subgraph, increment label */
+
+        /* FIND j */
+        j = 0;
+        while (c[j] + 1 == c[j+1]) { c[j] = j; j++; }
+
+        /* DONE? */
+        if (j == t) break;
+
+        c[j]++;
+    }
+}
+
+void free_tabs(int *sub[], int *edg[], int nsg)
+{
+    int i;
+
+    for (i = 0; i < NED; i++)
+        free(sub[i]);
+    for (i = 0; i < nsg; i++)
+        free(edg[i]);
+}
+
---
+++ ramsey.h
@@ -0,0 +1,63 @@
+/* Undefined constants (computed by compile script)
+ *      NV      : number of vertices
+ *      R       : red clique size
+ *      S       : blue clique size
+ *      NED     : number of edges (=NV(NV-1)/2)
+ *      NSGR    : number of subgraphs with R vertices (=binomial(NV, S))
+ *      NSGS    : number of subgraphs with S vertices (=binomial(NV, S))
+ *      NSGFER  : number of subgraphs with R vertices including a given edge
+ *      NSGFES  : number of subgraphs with S vertices including a given edge
+ *                  (=binomial(NV-2, S-2))
+ */
+
+#include "dSFMT.h"
+
+#if !(  defined(NV)     && defined(R)       && defined(S) &&\
+        defined(NED)    && defined(NSGR)    && defined(NSGS) &&\
+        defined(NSGFER) && defined(NSGFES))
+#error Missing required definitions. See dSFMT.h.
+#endif
+
+#define R_RAND() dsfmt_genrand_close_open(&rng_state)
+        
+/* Strucure to store the configuration of one replica */
+typedef struct
+{
+    int sp[NED];
+    int h2[NED];
+    int nbr[NSGR];  /* number of blue edges in each R-subgraph */
+    int nbs[NSGS];  /* number of blue edges in each S-subgraph */
+    int en;         /* number of blue S-cliques and red R-cliques */
+} rep_t;
+
+extern dsfmt_t rng_state;   /* state of random number generator (RNG) */
+
+/* Call R_init() first, and R_finalize() to free memory when done */
+void R_init(uint32_t seed);
+void R_finalize();
+
+/* Initialize replica with all edges blue */
+void R_init_replica(rep_t *p);  
+
+/*
+ * Initialize replica in a random configuration with equal numbers of red and
+ * blue edges on average
+ */
+void R_init_replica_random(rep_t *p);
+
+/*
+ * Initialize replica using configuration from a graph file. If the file
+ * specifies a graph with fewer than NV vertices, initialize the unspecified
+ * edges randomly with equal probabilities for red and blue.
+ */
+void R_init_replica_from_file(rep_t *p, char filename[]);
+
+/*
+ * Update local fields after a spin flip (called AFTER spin assigned new value)
+ *      ei                  : index of flipped spin
+ *      sp, nbr, nbs, h2    : values for the replica containing flipped spin
+ */
+void R_update_fields(int ei, int sp[], int nbr[], int nbs[], int h2[]);
+
+/* Save graph to file */
+void R_save_graph(int sp[], char filename[]);
