6d7c48916f4527503b9e6c35fee0633bb76d565f
Matt Wittmann <mcwitt@gmail.com>
2012-02-28 13:34:51 -0800

    Added some skeleton code for genetic algorithm

---
+++ genetic.c
@@ -0,0 +1,20 @@
+#define POPSIZE 100
+
+/* select individuals for mating */
+void select(int current[POPSIZE][], int selected[POPSIZE][])
+{
+}
+
+void mate(int selected[POPSIZE][], int next[POPSIZE][])
+{
+}
+
+/* do one-point crossover at position k */
+void crossover(int i1[], int i2[], int o1[], int o2[], int k)
+{
+}
+
+/* mutate string s with bit-flipping probability p_flip */
+void mutate(int s[], double p_flip)
+{
+}
